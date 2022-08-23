ff5604de8c2076ba701cb7d421873a91619b5386
radosz99 <241385@student.pwr.edu.pl>
2022-08-23 15:22:27 +0200

    Add logging for creating directory

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,10 @@
 import os
+import logger
 
 from .constants import API_RESPONSES_DIR
 
 try:
     os.mkdir(API_RESPONSES_DIR)
-except FileExistsError:
-    pass
+except Exception as e:
+    logger.get_logger().info(f"Cannot create {API_RESPONSES_DIR} directory because of - {e}")
+
